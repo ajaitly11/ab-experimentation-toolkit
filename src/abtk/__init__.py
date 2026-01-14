b8abfcb0ab1aaa1c4f62ace53e0f0739af31473a
Arnav Jaitly <arnavjaitly98@gmail.com>
2026-01-14 20:39:05 +0000

    feat: export power and sample size functions

--- src/abtk/__init__.py
+++ src/abtk/__init__.py
@@ -8,6 +8,22 @@ from .mean import mean_diff
 from .conversion import conversion_diff
 from .ratio import ratio_diff
 from .health import srm_check
+from .power import (
+    power_two_means,
+    power_two_proportions,
+    sample_size_two_means,
+    sample_size_two_proportions,
+)
 
-__all__ = ["__version__", "mean_diff", "conversion_diff", "ratio_diff", "srm_check"]
+__all__ = [
+    "__version__",
+    "mean_diff",
+    "conversion_diff",
+    "ratio_diff",
+    "srm_check",
+    "sample_size_two_proportions",
+    "power_two_proportions",
+    "sample_size_two_means",
+    "power_two_means",
+]
 __version__ = "0.0.0"
